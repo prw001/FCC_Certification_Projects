b0d17ccbc581526ffdaf279cd14f649bcba6de9b
prw001 <pwinter001@gmail.com>
2026-09-17 12:04:51 +0200

    create addOption and vote functions

---
+++ Build_a_Voting_System/Voting_Poll.js
@@ -0,0 +1,37 @@
+let poll = new Map();
+
+const addOption = (option) => {
+  if (Object.keys(option).length === 0)
+  {
+    return `Option cannot be empty`;
+  }
+  if (!poll.has(option))
+  {
+    poll.set(option, new Set());
+    return `Option ${option} added to the poll`;
+  }
+  else
+  {
+    return `Option ${option} already exists`;
+  }
+}
+
+const vote = (option, voterId) => {
+    if (!poll.has(option))
+    {
+        return `Option ${option} does not exist.`;
+    }
+    else
+    {
+        const voterIds = poll.get(option);
+        if (voterIds.has(voterId))
+        {
+            return `Voter ${voterId} has already voted for ${option}`;
+        }
+        else
+        {
+            voterIds.add(voterId);
+            return `Voter ${voterId} voted for "${option}".`;
+        }
+    }
+}
